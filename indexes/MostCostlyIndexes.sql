6a037007afad8b126b606b57c3ae5fed47da2816
James McCabe <jamesp.mccabe@outlook.com>
2018-05-29 18:47:05 +0100

    Create MostCostlyIndexes.sql

---
+++ indexes/MostCostlyIndexes.sql
@@ -0,0 +1,21 @@
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+SELECT TOP 100
+	DB_NAME() AS DatabaseName
+	, SCHEMA_NAME(o.Schema_ID) AS SchemaName
+	, OBJECT_NAME(s.[object_id]) AS [TableName]
+	, i.name AS IndexName
+	, s.user_updates
+	, s.system_seeks + s.system_scans + s.system_lookups AS [System usage]
+	, s.user_seeks + s.user_scans + s.user_lookups AS [User usage]
+FROM sys.dm_db_index_usage_stats s
+	INNER JOIN sys.indexes i ON s.[object_id] = i.[object_id] AND s.index_id = i.index_id
+	INNER JOIN sys.objects o ON i.object_id = O.object_id
+WHERE s.database_id = DB_ID()
+	AND OBJECTPROPERTY(s.[object_id], 'IsMsShipped') = 0
+	AND s.user_seeks < 10
+	AND s.user_scans < 10
+	AND s.user_lookups < 10
+	AND i.name IS NOT NULL
+	--AND i.name not like 'PK%'
+	--AND OBJECT_NAME(s.[object_id]) = 'tFTPLog'
+ORDER BY [tablename] --s.user_seeks + s.user_scans + s.user_lookups
